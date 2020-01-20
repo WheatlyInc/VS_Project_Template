81d69590b283d0c0048735eac81cb88e86ef0ca4
WheatlyInc <30851945+WheatlyInc@users.noreply.github.com>
2020-01-20 17:05:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VS_Project_Template
+ A project template to easily make a new C++ project with set Output Directory and Intermediate Directory settings in Visual Studio.
---
+++ VS_Project_Template/src/main.cpp
@@ -0,0 +1,20 @@
+/************************************
+*	Project Title Here
+*  John Fahringer
+*
+*  2020, All rights reserved.
+*
+*************************************/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+std::string input;
+
+int main()
+{
+	std::cout << "Hello World! I am a template!" << std::endl;
+	std::cin.ignore();
+}
